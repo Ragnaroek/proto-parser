85d88adc9d7d217d725992b4ff9842b639156882
Michael Bohn <michael.bohn@freiheit.com>
2017-07-11 22:28:47 +0200

    parse syntax stmt

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,35 @@
-use super::scanner::Scanner;
+use super::scanner::{Scanner, Token};
 use super::ast::{ProtoDef, Syntax};
 
 
 //TODO Impl recursive descend parser here :)
 
-pub fn parse(buffer: &String) -> Result<ProtoDef, &'static str> {
+pub fn parse(buffer: &String) -> Result<ProtoDef, &str> {
     let mut scanner = Scanner::new(buffer);
-    let tk = scanner.next_token();
 
-    println!("token: {:?}", tk);
+    syntax(&mut scanner)?;
+
+    //TODO parse list of top-level statements!
 
     return Ok(ProtoDef{syntax: Syntax::V3});
+}
+
+fn syntax(scanner: &mut Scanner) -> Result<Syntax, &'static str> {
+    expect(scanner, Token::Syntax)?;
+    expect(scanner, Token::Eq)?;
+    expect(scanner, Token::StrLit("proto3".to_string()))?;
+    return Ok(Syntax::V3);
+}
+
+fn expect(mut scanner: &mut Scanner, expected: Token) -> Result<Token, &'static str> {
+    let next = scanner.next_token();
+    if let Err(e) = next {
+        return Err(e);
+    }
+
+    let tk = next.unwrap();
+    if tk != expected {
+        return Err("unexpected token");
+    }
+    return Ok(tk);
 }
